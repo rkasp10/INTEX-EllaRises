5c8d7b2e458673275c5e6628c23c319555009f8c
mspulsi <mspulsipher@gmail.com>
2025-12-04 15:41:19 -0700

    make sure app gets aws port var

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const helmet = require("helmet"); // Security middleware
 const db = require("./db"); // Shared database connection
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // ---------------------------------------------
 // Middleware
